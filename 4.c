4a3b668ebebf8efab3738fc484c4b37459e16626
Radhikakrishna1 <radhikakrishna443@gmail.com>
2023-11-09 15:53:53 +0530

    mathh

--- 4.c
+++ 4.c
@@ -1,4 +1,20 @@
-/*
+/
+*The integer datatype in C is used to store the integer numbers(any number including positive, negative and zero without decimal part).
+ Octal values, hexadecimal values, and decimal values can be stored in int data type in C. 
+
+Range:  -2,147,483,648 to 2,147,483,647
+Size: 4 bytes
+Format Specifier: %d
+
+Syntax of Integer
+
+We use int keyword to declare the integer variable:
+
+int var_name;
+*/
+// C program to demonstrate Three variables of Relational Operators
+// C using three variables of Relatinal Operators.
+
 
 #include <stdio.h>
 int main()
--- 9.c
+++ 9.c
@@ -2,6 +2,20 @@
 The perimeter of a rectangle is e total distance of its outer boundary. 
 It is twice the sum of its length and width and it is calculated with the help of the formula:
  Perimeter = 2(length + width).
+
+ Integer Data Type
+The integer datatype in C is used to store the integer numbers(any number including positive, negative and zero without decimal part).
+ Octal values, hexadecimal values, and decimal values can be stored in int data type in C. 
+
+Range:  -2,147,483,648 to 2,147,483,647
+Size: 4 bytes
+Format Specifier: %d
+
+Syntax of Integer
+
+We use int keyword to declare the integer variable:
+
+int var_name;
 */
 //  c  program with Perimeter of  rectangle.
 
